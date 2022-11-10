a7380befce6defe9e4e58d71c136d4be0b6f8c1f
angelicaaeun <aeun@usc.edu>
2022-11-09 20:54:01 -0800

    add project 3

---
+++ project-3/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {   
+    $('.plant1').click(function () {
+      $('.text1').toggle();
+    });
+    
+//     $('#imgage1').click(function () {
+//        $('.infopanel').toggle(2000);     
+//        $(this).toggleClass("bigger");
+//    });iml300
+//    
+});
