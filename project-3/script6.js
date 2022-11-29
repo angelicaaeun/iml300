2006d019c9cdb7e49a464433acad2a04183a6ac6
angelicaaeun <aeun@usc.edu>
2022-11-29 04:36:56 -0800

    edit -- page 6
    up to page 6

---
+++ project-3/script6.js
@@ -0,0 +1,40 @@
+//$(function () {
+//        $(".groceries").draggable();
+//    });
+//
+//    $('#sugar').click(function () {
+//        $('.pop-up1').toggle();
+//    });
+//
+//    $("#toast").click(function () {
+//        //        $("#plant4").hide();
+//        $("#pop-up1").toggle();
+//    });
+//
+//    $('#earphones').draggable();
+//    });
+//
+//    $('#computer2').hover(function () {
+//        $('#p3').fadeIn();
+//    }, function () {
+//        $('#p3').fadeOut();
+//    });
+//
+//    $('#computer3').hover(function () {
+//        $('#p4').fadeIn();
+//    }, function () {
+//        $('#p4').fadeOut();
+//    });
+
+$(document).ready(function () {   
+    $('button').click(function () {
+      $('.announcement').toggle();
+    });
+    });
+    
+//     $('#imgage1').click(function () {
+//        $('.infopanel').toggle(2000);     
+//        $(this).toggleClass("bigger");
+//    });
+    
+
